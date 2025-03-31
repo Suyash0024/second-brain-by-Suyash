a3771aa34151ec4c01ac227e0d668170e908c103
Suyash Garud <113934597+Suyash0024@users.noreply.github.com>
2025-03-31 12:31:17 +0530

    Update App.tsx

--- second-brain-1-frontend/src/App.tsx
+++ second-brain-1-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Home } from "./pages/Home";
+import { Home } from "./pages/home";
 import { Signup } from "./pages/Signup"
 import { Signin } from "./pages/Signin"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
